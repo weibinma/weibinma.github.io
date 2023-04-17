ec6585e65a68987f4d36fd9de74b1c391f24bc9a
hello_world_gogogo <weibinma0402@gmail.com>
2023-04-17 11:48:12 -0400

    made aboutme section to support vuejs and  changed all double quotes in js to single quote

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const headers = Vue.createApp({
     data() {
         return {
             university: [
-                {id: "cis", name: "Department of Computer & Information Sciences", url: "https://www.cis.udel.edu"},
-                {id: "udel", name: "University of Delaware", url: "https://www.udel.edu"},
+                {id: 'cis', name: 'Department of Computer & Information Sciences', url: 'https://www.cis.udel.edu'},
+                {id: 'udel', name: 'University of Delaware', url: 'https://www.udel.edu'},
             ],
             myself: {
-                linkedIn: {id: "linkedIn", name: "LinkedIn", url: "https://www.linkedin.com/in/weibinma"},
-                googleScholar: {id: "googleScholar", name: "Google Scholar", url: "https://scholar.google.com/citations?user=Wr8AlW8AAAAJ&hl=en"},
-                gitHub: {id: "gitHub", name: "GitHub", url: "https://github.com/weibinma"},
+                linkedIn: {id: 'linkedIn', name: 'LinkedIn', url: 'https://www.linkedin.com/in/weibinma'},
+                googleScholar: {id: 'googleScholar', name: 'Google Scholar', url: 'https://scholar.google.com/citations?user=Wr8AlW8AAAAJ&hl=en'},
+                gitHub: {id: 'gitHub', name: 'GitHub', url: 'https://github.com/weibinma'},
             }
         }
     },
@@ -16,13 +16,25 @@ const headers = Vue.createApp({
 
 headers.mount('#headers');
 
+
+const aboutme = Vue.createApp({
+    data() {
+        return {
+            text: 'I am currently a Ph.D. Candidate in Computer Science at University of Delaware, where I am fortunate to be advised by <a target="_blank" href="https://www.eecis.udel.edu/lena/">Prof. Lena Mashayekhy</a>. Before entering PhD study in fall 2018, I received my B.S. degree in Electronic Information Science and Technology at Dalian Maritime University, Dalian, China in 2014 and received my M.S. degree in Data Science at University at Buffalo, The State University of New York, Buffalo, USA in 2018.'
+        }
+    },
+});
+
+aboutme.mount('#about-me');
+
+
 const work = Vue.createApp({
     data() {
         return {
             works: [
-                {id: "intern1", company: "Meta (Facebook)", title: "Software Engineer Intern, Systems and Infrastructure (PhD)", date: "Jun 2022 - Sep 2022", location: "Menlo Park, CA"},
-                {id: "intern2", company: "Marion Surgical", title: "Data Engineer Intern", date: "May 2018 - Aug 2018", location: "Buffalo, NY"},
-                {id: "fulltime1", company: "Tianma Microelectronics", title: "Test Device Engineer", date: "July 2014 - Feb 2016", location: "Xiamen, China"}
+                {id: 'intern1', company: 'Meta (Facebook)', title: 'Software Engineer Intern, Systems and Infrastructure (PhD)', date: 'Jun 2022 - Sep 2022', location: 'Menlo Park, CA'},
+                {id: 'intern2', company: 'Marion Surgical', title: 'Data Engineer Intern', date: 'May 2018 - Aug 2018', location: 'Buffalo, NY'},
+                {id: 'fulltime1', company: 'Tianma Microelectronics', title: 'Test Device Engineer', date: 'July 2014 - Feb 2016', location: 'Xiamen, China'}
             ]
         }
     },
@@ -30,72 +42,73 @@ const work = Vue.createApp({
 
 work.mount('#work-experience');
 
+
 const publication = Vue.createApp({
     data() {
         return {
             journal: [{
-                id: "tsc2022", 
-                title: "Time-Constrained Service Handoff for Mobile Edge Computing in 5G", 
-                authors: "Nafiseh Sharghivand, Lena Mashayekhy, <strong>Weibin Ma</strong>, Schahram Dustdar", 
-                publisher: "IEEE Transactions on Services Computing, Sep 2022 (<strong>Impact Factor: 11.019</strong>)", 
-                url: "https://ieeexplore.ieee.org/document/9899742", 
-                pdf:"./files/tsc_2022.pdf"}
+                id: 'tsc2022', 
+                title: 'Time-Constrained Service Handoff for Mobile Edge Computing in 5G', 
+                authors: 'Nafiseh Sharghivand, Lena Mashayekhy, <strong>Weibin Ma</strong>, Schahram Dustdar', 
+                publisher: 'IEEE Transactions on Services Computing, Sep 2022 (<strong>Impact Factor: 11.019</strong>)', 
+                url: 'https://ieeexplore.ieee.org/document/9899742', 
+                pdf: './files/tsc_2022.pdf'}
             ], 
             conference: [{
-                id: "ucc2021", 
-                title: "QoS-Aware 5G Component Selection for Content Delivery in Multi-access Edge Computing", 
-                authors: "Erfan Farhangi Maleki, <strong>Weibin Ma</strong>, Lena Mashayekhy and Humberto La Roche", 
-                publisher: "IEEE/ACM UCC, Leicester, UK, Dec 2021", 
-                url: "https://dl.acm.org/doi/10.1145/3468737.3494101", 
-                pdf: ""
+                id: 'ucc2021', 
+                title: 'QoS-Aware 5G Component Selection for Content Delivery in Multi-access Edge Computing', 
+                authors: 'Erfan Farhangi Maleki, <strong>Weibin Ma</strong>, Lena Mashayekhy and Humberto La Roche', 
+                publisher: 'IEEE/ACM UCC, Leicester, UK, Dec 2021', 
+                url: 'https://dl.acm.org/doi/10.1145/3468737.3494101', 
+                pdf: ''
             },
             {
-                id: "cloud2021", 
-                title: "Quality-Aware Video Offloading in Mobile Edge Computing: A Data-driven Two-stage Stochastic Optimization", 
-                authors: "<strong>Weibin Ma</strong> and Lena Mashayekhy", 
-                publisher: "IEEE CLOUD, Virtual, September 2021", 
-                url: "https://ieeexplore.ieee.org/document/9582261", 
-                pdf: "./files/cloud_2021.pdf"
+                id: 'cloud2021', 
+                title: 'Quality-Aware Video Offloading in Mobile Edge Computing: A Data-driven Two-stage Stochastic Optimization', 
+                authors: '<strong>Weibin Ma</strong> and Lena Mashayekhy', 
+                publisher: 'IEEE CLOUD, Virtual, September 2021', 
+                url: 'https://ieeexplore.ieee.org/document/9582261', 
+                pdf: './files/cloud_2021.pdf'
             },
             {
-                id: "icdcs2021", 
-                title: "Poster: Adaptive Video Offloading in Mobile Edge Computing", 
-                authors: "<strong>Weibin Ma</strong> and Lena Mashayekhy", 
-                publisher: "IEEE ICDCS, Virtual, July 2021", 
-                url: "https://ieeexplore.ieee.org/document/9546493", 
-                pdf: "./files/icdcs_poster_2021.pdf"
+                id: 'icdcs2021', 
+                title: 'Poster: Adaptive Video Offloading in Mobile Edge Computing', 
+                authors: '<strong>Weibin Ma</strong> and Lena Mashayekhy', 
+                publisher: 'IEEE ICDCS, Virtual, July 2021', 
+                url: 'https://ieeexplore.ieee.org/document/9546493', 
+                pdf: './files/icdcs_poster_2021.pdf'
             },
             {
-                id: "edgecom2020", 
-                title: "Truthful Computation Offloading Mechanisms for Edge Computing", 
-                authors: "<strong>Weibin Ma</strong> and Lena Mashayekhy", 
-                publisher: "IEEE EdgeCom, Virtual, August 2020", 
-                url: "https://ieeexplore.ieee.org/document/9170974", 
-                pdf: "./files/edgecom_2020.pdf"
+                id: 'edgecom2020', 
+                title: 'Truthful Computation Offloading Mechanisms for Edge Computing', 
+                authors: '<strong>Weibin Ma</strong> and Lena Mashayekhy', 
+                publisher: 'IEEE EdgeCom, Virtual, August 2020', 
+                url: 'https://ieeexplore.ieee.org/document/9170974', 
+                pdf: './files/edgecom_2020.pdf'
             },
             {
-                id: "ucc2019", 
-                title: "Privacy-by-Design Distributed Offloading for Vehicular Edge Computing", 
-                authors: "<strong>Weibin Ma</strong> and Lena Mashayekhy", 
-                publisher: "IEEE/ACM UCC, Auckland, New Zealand, December 2019", 
-                url: "https://dl.acm.org/doi/10.1145/3344341.3368804", 
-                pdf: ""
+                id: 'ucc2019', 
+                title: 'Privacy-by-Design Distributed Offloading for Vehicular Edge Computing', 
+                authors: '<strong>Weibin Ma</strong> and Lena Mashayekhy', 
+                publisher: 'IEEE/ACM UCC, Auckland, New Zealand, December 2019', 
+                url: 'https://dl.acm.org/doi/10.1145/3344341.3368804', 
+                pdf: ''
             },
             {
-                id: "sec2019", 
-                title: "Poster: Privacy-by-design Task Offloading for UAV-mounted Cloudlets", 
-                authors: "<strong>Weibin Ma</strong> and Lena Mashayekhy", 
-                publisher: "ACM/IEEE SEC, Arlington VA, USA, November 2019", 
-                url: "https://dl.acm.org/doi/10.1145/3318216.3363320", 
-                pdf: ""
+                id: 'sec2019', 
+                title: 'Poster: Privacy-by-design Task Offloading for UAV-mounted Cloudlets', 
+                authors: '<strong>Weibin Ma</strong> and Lena Mashayekhy', 
+                publisher: 'ACM/IEEE SEC, Arlington VA, USA, November 2019', 
+                url: 'https://dl.acm.org/doi/10.1145/3318216.3363320', 
+                pdf: ''
             },
             {
-                id: "iciot2019", 
-                title: "A Strategic Game for Task Offloading among Capacitated UAV-Mounted Cloudlets", 
-                authors: "<strong>Weibin Ma</strong>, Xuanzhang Liu, and Lena Mashayekhy", 
-                publisher: "IEEE ICIOT, Milan, Italy, July 2019", 
-                url: "https://ieeexplore.ieee.org/document/8815659", 
-                pdf: "./files/iciot_2019.pdf"
+                id: 'iciot2019', 
+                title: 'A Strategic Game for Task Offloading among Capacitated UAV-Mounted Cloudlets', 
+                authors: '<strong>Weibin Ma</strong>, Xuanzhang Liu, and Lena Mashayekhy', 
+                publisher: 'IEEE ICIOT, Milan, Italy, July 2019', 
+                url: 'https://ieeexplore.ieee.org/document/8815659', 
+                pdf: './files/iciot_2019.pdf'
             }
         ]
     }
@@ -103,14 +116,15 @@ const publication = Vue.createApp({
 
 publication.mount('#publication');
 
+
 const award = Vue.createApp({
     data() {
         return {
             awards: [
-                {id: "cis2021", awardName: "CIS Outstanding Graduate Student Award, University of Delaware, 2021", url: "https://www.cis.udel.edu/2021/05/14/congratulations-to-the-2021-cis-honors-day-award-recipients"},
-                {id: "cis2020", awardName:"CIS Distinguished Graduate Student Award, University of Delaware, 2020", url:"https://www.cis.udel.edu/2020/05/28/congratulations-to-2020-cis-honors-day-award-recipients"},
-                {id: "pda2019", awardName:"Professional Development Award, University of Delaware, 2019", url:""},
-                {id: "nsfsec2019", awardName:"NSF Student Travel Grant, ACM/IEEE SEC, 2019", url:""}
+                {id: 'cis2021', awardName: 'CIS Outstanding Graduate Student Award, University of Delaware, 2021', url: 'https://www.cis.udel.edu/2021/05/14/congratulations-to-the-2021-cis-honors-day-award-recipients'},
+                {id: 'cis2020', awardName: 'CIS Distinguished Graduate Student Award, University of Delaware, 2020', url: 'https://www.cis.udel.edu/2020/05/28/congratulations-to-2020-cis-honors-day-award-recipients'},
+                {id: 'pda2019', awardName: 'Professional Development Award, University of Delaware, 2019', url: ''},
+                {id: 'nsfsec2019', awardName: 'NSF Student Travel Grant, ACM/IEEE SEC, 2019', url: ''}
             ]
         }
     }
@@ -122,9 +136,9 @@ const service = Vue.createApp({
     data() {
         return {
             services: [
-                {id: "coordination2022", confName: "Subreviewer in the 24th International Conference on Coordination Models and Languages (COORDINATION 2022)"},
-                {id: "fmec2021", confName: "TPC in the 6th International Conference on Fog and Mobile Edge Computing (FMEC 2021)"},
-                {id: "fmec2020", confName: "TPC in the 5th International Conference on Fog and Mobile Edge Computing (FMEC 2020)"}
+                {id: 'coordination2022', confName: 'Subreviewer in the 24th International Conference on Coordination Models and Languages (COORDINATION 2022)'},
+                {id: 'fmec2021', confName: 'TPC in the 6th International Conference on Fog and Mobile Edge Computing (FMEC 2021)'},
+                {id: 'fmec2020', confName: 'TPC in the 5th International Conference on Fog and Mobile Edge Computing (FMEC 2020)'}
             ]
         }
     },
